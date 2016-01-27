71a5f162eaace0555c575a89de1dbbdc272decf1
Markus Birth <mbirth@gmail.com>
2016-01-27 12:46:57 +0100

    Tabs to spaces.

--- geolog2gpx.sh
+++ geolog2gpx.sh
@@ -17,59 +17,59 @@ for i in $CACHE_FOLDERS ;
 
 do
 
-	# Hat der Cache eine OC-ID?
-	if [ "$(grep "GCid" $i/cache.txt | awk '{print $2}')" = "" ] ; then
-		echo "Kein GC-Cache, überspringe Eintrag!"
-	else
-		ID=$(grep "GCid" $i/cache.txt | awk '{print $2}')
-		# ID steht, also noch die anderen Werte aus der cache.txt holen.
-		NAME=$(grep "Name" $i/cache.txt | awk '{print $2}')
-		LAT=$(grep "Lat" $i/cache.txt | awk '{print $2}')
-		LON=$(grep "Lon" $i/cache.txt | awk '{print $2}')
+    # Hat der Cache eine OC-ID?
+    if [ "$(grep "GCid" $i/cache.txt | awk '{print $2}')" = "" ] ; then
+        echo "Kein GC-Cache, überspringe Eintrag!"
+    else
+        ID=$(grep "GCid" $i/cache.txt | awk '{print $2}')
+        # ID steht, also noch die anderen Werte aus der cache.txt holen.
+        NAME=$(grep "Name" $i/cache.txt | awk '{print $2}')
+        LAT=$(grep "Lat" $i/cache.txt | awk '{print $2}')
+        LON=$(grep "Lon" $i/cache.txt | awk '{print $2}')
 
-		# Beim Cachetyp hat der korrekte Name Leerzeichen, also nacharbeiten.
-		if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Traditional" ] ; then
-			TYPE="Traditional Cache"
-		else
-			if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Virtual" ] ; then
-				TYPE="Virtual Cache"
-			else
-				if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Event" ] ; then
-					TYPE="Event Cache"
-				else
-					if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Webcam" ] ; then
-						TYPE="Webcam Cache"
-					else
-						if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Unknown" ] ; then
-							TYPE="Unknown Cache"
-						else
-							if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Letterbox" ] ; then
-							TYPE="Letterbox hybrid"
-							else
-								# Nur der Multi steht schon richtig in der Variable. ;)
-								TYPE=$(grep "Type" $i/cache.txt | awk '{print $2}')
-							fi
-						fi
-					fi
-				fi
-			fi
-		fi
-		# Und noch den Rest einsammeln.
-		CONTAINER=$(grep "Container:" $i/cache.txt | awk '{print $2}')
-		DIFF=$(grep "Difficulty" $i/cache.txt | awk '{print $2}')
-		TERR=$(grep "Terrain" $i/cache.txt | awk '{print $2}')
-		# Ab damit ins GPX.
-		echo "Bearbeite: "$ID;
-		echo '	<wpt lat="'$LAT'" lon="'$LON'">' >> ../../$FILENAME;
-		echo '		<name>'$ID'</name>' >> ../../$FILENAME;
-    	echo '	    <groundspeak:cache>' >> ../../$FILENAME;
-		echo '	      	<groundspeak:name>'$NAME'</groundspeak:name>' >> ../../$FILENAME;
-		echo '	      	<groundspeak:type>'$TYPE'</groundspeak:type>' >> ../../$FILENAME;
-		echo '	      	<groundspeak:difficulty>'$DIFF'</groundspeak:difficulty>' >> ../../$FILENAME;
-		echo '	      	<groundspeak:terrain>'$TERR'</groundspeak:terrain>' >> ../../$FILENAME;
-		echo '	    </groundspeak:cache>' >> ../../$FILENAME;
-		echo '	</wpt>' >> ../../$FILENAME;
-	fi
+        # Beim Cachetyp hat der korrekte Name Leerzeichen, also nacharbeiten.
+        if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Traditional" ] ; then
+            TYPE="Traditional Cache"
+        else
+            if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Virtual" ] ; then
+                TYPE="Virtual Cache"
+            else
+                if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Event" ] ; then
+                    TYPE="Event Cache"
+                else
+                    if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Webcam" ] ; then
+                        TYPE="Webcam Cache"
+                    else
+                        if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Unknown" ] ; then
+                            TYPE="Unknown Cache"
+                        else
+                            if [ $(grep "Type" $i/cache.txt | awk '{print $2}') = "Letterbox" ] ; then
+                            TYPE="Letterbox hybrid"
+                            else
+                                # Nur der Multi steht schon richtig in der Variable. ;)
+                                TYPE=$(grep "Type" $i/cache.txt | awk '{print $2}')
+                            fi
+                        fi
+                    fi
+                fi
+            fi
+        fi
+        # Und noch den Rest einsammeln.
+        CONTAINER=$(grep "Container:" $i/cache.txt | awk '{print $2}')
+        DIFF=$(grep "Difficulty" $i/cache.txt | awk '{print $2}')
+        TERR=$(grep "Terrain" $i/cache.txt | awk '{print $2}')
+        # Ab damit ins GPX.
+        echo "Bearbeite: "$ID;
+        echo '  <wpt lat="'$LAT'" lon="'$LON'">' >> ../../$FILENAME;
+        echo '      <name>'$ID'</name>' >> ../../$FILENAME;
+        echo '      <groundspeak:cache>' >> ../../$FILENAME;
+        echo '          <groundspeak:name>'$NAME'</groundspeak:name>' >> ../../$FILENAME;
+        echo '          <groundspeak:type>'$TYPE'</groundspeak:type>' >> ../../$FILENAME;
+        echo '          <groundspeak:difficulty>'$DIFF'</groundspeak:difficulty>' >> ../../$FILENAME;
+        echo '          <groundspeak:terrain>'$TERR'</groundspeak:terrain>' >> ../../$FILENAME;
+        echo '      </groundspeak:cache>' >> ../../$FILENAME;
+        echo '  </wpt>' >> ../../$FILENAME;
+    fi
 
 done
 
