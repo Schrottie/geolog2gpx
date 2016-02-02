822a00df4cd9cf0a16356f23c199ac1da9ec0d1d
Schrottie <maik@dafb-o.de>
2016-02-02 08:12:25 +0100

    Optionen für OConly, GConly oder beides

--- geolog2gpx.sh
+++ geolog2gpx.sh
@@ -1,10 +1,66 @@
 #!/bin/bash
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+#
+# Author: Schrottie, mbirth
+
+usage() {
+echo "$@ [-o] [-g] [-b] [-l] [-h]"
+echo " Erzeugen eines GPX-File aus dem Datenbestand der Perlprogramme"
+echo " geolog und ocprop."
+echo "Optionen:"
+echo " -o Nur Geocaches von Opencaching.de berücksichtigen."
+echo " -g Nur Geocaches von Geocaching.com berücksichtigen."
+echo " -b Alle Geocaches berücksichtigen."
+echo " -h Diese Hilfe anzeigen."
+}
+
+OCONLY=0
+GCONLY=0
+BOTHOCGC=0
+
+while getopts ogbh OPT; do
+case "$OPT" in
+        h)
+            usage $(basename $0)
+            exit 0
+            ;;
+        o)
+            OCONLY=1
+            ;;
+        g)
+            GCONLY=1
+            ;;
+        b)
+            BOTHOCGC=1
+            ;;
+        \?)
+            usage $(basename $0) >&2
+            exit 1
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
 
 if [ -z "$1" ]; then
     GEOLOG_PATH="/home/schrottie/Google Drive/geolog/gcdir/found"
 else
     GEOLOG_PATH="$1"
 fi
+
 
 # GPX erzeugen
 FILENAME=`date +%Y-%m-%d-%H-%M`.gpx
@@ -25,13 +81,32 @@ echo '  <time>'$GPXDATE'</time>' >> ./$FILENAME;
 find "$GEOLOG_PATH" -mindepth 1 -maxdepth 1 -type d -print0 | \
 while IFS= read -r -d '' i; do
 
-    ID=$(grep -a "GCid:" "$i/cache.txt" | awk '{print $2}')
+	if [ $GCONLY == 1 ] ; then
+		# Nur Caches mit GC-ID berücksichtigen!
+		ID=$(grep -a "GCid:" "$i/cache.txt" | awk '{print $2}')
+		if [ "$ID" = "" ] ; then
+			echo "Kein GC-Cache: $i, überspringe Eintrag!"
+			continue
+		fi
+	else
+	if [ $OCONLY == 1 ] ; then
+		# Nur Caches mit OC-ID berücksichtigen!
+		ID=$(grep -a "OCId:" "$i/note.txt" | awk '{print $2}')
+		if [ "$ID" = "" ] ; then
+			echo "Kein OC-Cache: $i, überspringe Eintrag!"
+			continue
+		fi
+	else
+	if [ $BOTHOCGC == 1 ] ; then
+		# Alle Caches berücksichtigen!
+		ID=$(grep -a "GCid:" "$i/cache.txt" | awk '{print $2}')
+		if [ "$ID" = "" ] ; then
+			ID=$(grep -a "OCId:" "$i/note.txt" | awk '{print $2}')
+		fi
+	fi
+	fi
+	fi
 
-    # Hat der Cache eine OC-ID?
-    if [ "$ID" = "" ] ; then
-        echo "Kein GC-Cache: $i, überspringe Eintrag!"
-        continue
-    fi
     # ID steht, also noch die anderen Werte aus der cache.txt holen.
     NAME=$(grep -a "Name:" "$i/cache.txt" | cut -d' ' -f2-)
     LAT=$(grep -a "Lat:" "$i/cache.txt" | awk '{print $2}')
@@ -42,7 +117,7 @@ while IFS= read -r -d '' i; do
     TERR=$(grep -a "Terrain:" "$i/cache.txt" | awk '{print $2}')
     
     # Ab damit ins GPX.
-    echo "Bearbeite: $ID ($i)";
+    echo "Bearbeite: $ID";
     echo '  <wpt lat="'$LAT'" lon="'$LON'">' >> ./$FILENAME;
     echo '      <name>'$ID'</name>' >> ./$FILENAME;
     echo '      <groundspeak:cache>' >> ./$FILENAME;
@@ -57,3 +132,4 @@ done
 
 # Und das GPX ordnungsgemäß zumachen.
 echo '</gpx>' >> ./$FILENAME;
+
